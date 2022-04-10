95924a849fc789fc6f659364098b3ddcb01c32a7
Jake Enger <99782876+jake-enger@users.noreply.github.com>
2022-04-10 13:14:31 -0700

    Added some new features/buttons! Also I was watching Severance while i did this, you guys should absolutely check it out.

--- server/index.js
+++ server/index.js
@@ -22,4 +22,37 @@ app.get("/api/compliment", (req, res) => {
   
 });
 
+app.get("/api/minorInsults", (req, res) => {
+  const minorInsults = ["Your hair looks small.",
+					 "Shut up, bird!",
+					 "Your Javascript skills are stellar, your HTML and CSS skills on the other hand...",
+  ];
+
+  // choose random insult
+  let randomIndex = Math.floor(Math.random() * minorInsults.length);
+  let randomInsults = minorInsults[randomIndex];
+
+  res.status(200).send(randomInsults);
+  
+});
+
+//new game recommendation, except its mostly Elden Ring with two other recommendations lol
+
+app.get("/api/games", (req, res) => {
+  const games = ["Elden Ring",
+            "Elden Ring, again",
+            "Guess what! Elden Ring again, scrub.",
+            "Super Smash Brothers Melee",
+            "Fine, you should play DOOM: ETERNAL"
+          ];
+
+  let randomIndex = Math.floor(Math.random() * games.length);
+  let randomGame = games[randomIndex];
+
+  res.status(200).send(randomGame)
+});
+
+
+
+
 app.listen(4000, () => console.log("Server running on 4000"));
